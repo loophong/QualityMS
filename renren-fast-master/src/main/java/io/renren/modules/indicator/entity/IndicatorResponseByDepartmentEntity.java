f8252c8a3e18cb7b62bcfdc6aac1ad0c1b8a63d4
Kanna Bush <lk644606288@163.com>
2024-09-23 17:36:25 +0800

    8-28指标模块修改

---
+++ renren-fast-master/src/main/java/io/renren/modules/indicator/entity/IndicatorResponseByDepartmentEntity.java
@@ -0,0 +1,6 @@
+package io.renren.modules.indicator.entity;
+
+public class IndicatorResponseByDepartmentEntity {
+    private String managemantDepartment;
+    private Integer counts;
+}
--- vue/src/views/common/home.vue
+++ vue/src/views/common/home.vue
@@ -1,39 +1,116 @@
 <template>
   <div class="mod-home">
-    <h3>项目介绍</h3>
-    <ul>
-      <li>renren-fast-vue基于vue、element-ui构建开发，实现<a href="https://gitee.com/renrenio/renren-fast" target="_blank">renren-fast</a>后台管理前端功能，提供一套更优的前端解决方案</li>
-      <li>前后端分离，通过token进行数据交互，可独立部署</li>
-      <li>主题定制，通过scss变量统一一站式定制</li>
-      <li>动态菜单，通过菜单管理统一管理访问路由</li>
-      <li>数据切换，通过mock配置对接口数据／mock模拟数据进行切换</li>
-      <li>发布时，可动态配置CDN静态资源／切换新旧版本</li>
-      <li>演示地址：<a href="http://demo.open.renren.io/renren-fast" target="_blank">http://demo.open.renren.io/renren-fast</a> (账号密码：admin/admin)</li>
-    </ul>
-    <h3>获取帮助</h3>
-    <ul>
-      <li>官方社区：<a href="https://www.renren.io/community" target="_blank">https://www.renren.io/community</a></li>
-      <li>前端Git地址：<a href="https://github.com/renrenio/renren-fast-vue" target="_blank">https://github.com/renrenio/renren-fast-vue</a></li>
-      <li>后台Git地址：<a href="https://gitee.com/renrenio/renren-fast" target="_blank">https://gitee.com/renrenio/renren-fast</a></li>
-      <li>代码生成器：<a href="https://gitee.com/renrenio/renren-generator" target="_blank">https://gitee.com/renrenio/renren-generator</a></li>
-      <li>如需关注项目最新动态，请Watch、Star项目，同时也是对项目最好的支持</li>
-    </ul>
-    <h3>官方QQ群</h3>
-    <ul>
-      <li>高级群：324780204(大牛云集，跟大牛学习新技能)</li>
-      <li>普通群：145799952(学习交流，互相解答各种疑问)</li>
-    </ul>
+    <div id="indicator" class="box">
+      <div>
+        指标总数：{{indicatorCounts}}
+      </div>
+      <div id="indicatorChart">
+
+      </div>
+    </div>
+    <div id="task" class="box">区域2</div>
+    <div id="issue" class="box">区域3</div>
+    <div id="QC" class="box">区域4</div>
   </div>
 </template>
 
+
 <script>
-  export default {
+import * as echarts from "echarts";
+export default {
+  name: 'home',
+  data () {
+    return {
+      //---------------------指标模块-------------------------
+      indicatorCounts: 0,
+      myIndicatorChart1: {}, //指标柱状图
+      departmentList: ["质量科", "市场科", "企业管理科"], //部门列表
+    }
+  },
+  mounted() {
+    this.getIndicatorCounts()
+    this.renderChart()
+
+  },
+  methods: {
+    //---------------------指标模块-------------------------
+    //查询指标总数
+    getIndicatorCounts() {
+      this.$http({
+        url: this.$http.adornUrl('/indicator/indicatordictionary/list02'),
+        method: 'get',
+        params: this.$http.adornParams({
+        })
+      }).then(({data}) => {
+        if (data && data.code === 0) {
+          this.indicatorCounts = data.page.totalCount
+        } else {
+          this.indicatorCounts = 0
+        }
+      })
+    },
+    renderChart() {
+      console.log('renderChart===>',this.selectedDate);
+      const chart = echarts.init(document.getElementById("indicatorChart"));
+
+      const option = {
+        title: {
+          text: "指标汇总",
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          data: this.departmentList,
+        },
+        yAxis: {
+          type: 'value',
+        },
+        series: [
+          {
+            name: '值',
+            type: 'bar',
+            data: this.chartData.map(item => item.value),
+            itemStyle: {
+              color: '#409EFF',
+            },
+          },
+        ],
+      };
+
+      chart.setOption(option);
+    },
+
   }
+}
+
 </script>
 
 <style>
   .mod-home {
-    line-height: 1.5;
+    display: flex;
+    flex-wrap: wrap; /* 自动换行 */
+    gap: 20px; /* 每个区域之间的间距 */
+  }
+  .box {
+    font-size: 24px; /* 设置字体大小 */
+    flex: 1 1 40%; /* 每个区域占 30% 宽度 */
+    //background-color: #f2f2f2;
+    text-align: center;
+    padding: 20px;
+    border-radius: 4px;
+    min-width: 200px; /* 设置最小宽度，防止过小 */
+    height: 400px; /* 设置每个容器的高度为父容器的百分比 */
   }
 </style>
 

36a87886548803d48759c020999b65df1789697a
Jane-Czh <1265909460@qq.com>
2024-10-15 16:59:42 +0800

    0925 仪器仪表 导入和警告功能

--- renren-fast-master/src/main/java/io/renren/modules/publicmanagement/entity/InstrumentLedgerEntity.java
+++ renren-fast-master/src/main/java/io/renren/modules/publicmanagement/entity/InstrumentLedgerEntity.java
@@ -1,5 +1,6 @@
 package io.renren.modules.publicmanagement.entity;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
@@ -27,58 +28,72 @@ public class InstrumentLedgerEntity implements Serializable {
 	/**
 	 * 公司编号
 	 */
+	@ExcelProperty(index = 0)
 	private String companyId;
 	/**
 	 * 级别
 	 */
+	@ExcelProperty(index = 1)
 	private String level;
 	/**
 	 * 检定周期
 	 */
+	@ExcelProperty(index = 2)
 	private Integer calibrationCycle;
 	/**
 	 * 检定状态
 	 */
+	@ExcelProperty(index = 3)
 	private String calibrationStatus;
 	/**
 	 * 使用状态
 	 */
+	@ExcelProperty(index = 4)
 	private String usageStatus;
 	/**
 	 * 配置地点
 	 */
+	@ExcelProperty(index = 5)
 	private String location;
 	/**
 	 * 责任部门
 	 */
+	@ExcelProperty(index = 6)
 	private String responsibleDepartment;
 	/**
 	 * 使用人
 	 */
+	@ExcelProperty(index = 7)
 	private String user;
 	/**
 	 * 检校日期
 	 */
-	private String calibrationDate;
+	@ExcelProperty(index = 8)
+	private Date calibrationDate;
 	/**
 	 * 检校有效期
 	 */
-	private String calibrationValidity;
+	@ExcelProperty(index = 9)
+	private Date calibrationValidity;
 	/**
 	 * 检校单位
 	 */
+	@ExcelProperty(index = 10)
 	private String calibrationUnit;
 	/**
 	 * 证书编号
 	 */
+	@ExcelProperty(index = 11)
 	private String certificateNumber;
 	/**
 	 * 备注
 	 */
+	@ExcelProperty(index = 12)
 	private String remark;
 	/**
 	 * 数量
 	 */
+	@ExcelProperty(index = 13)
 	private Integer quantity;
 	/**
 	 * 逻辑删除字段
--- vue/src/views/modules/publicmanagement/instrumentledger.vue
+++ vue/src/views/modules/publicmanagement/instrumentledger.vue
@@ -1,5 +1,12 @@
 <template>
+
+  
   <div class="mod-config">
+    <!-- 在模板中添加告警信息的展示 -->
+<div v-for="alert in alerts" :key="alert.numberId" @click="acknowledgeAlert(alert)" class="alert">
+  仪器{{ alert.numberId }}临期了！
+</div>
+
     <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
       <el-form-item>
         <el-input v-model="dataForm.key" placeholder="参数名" clearable></el-input>
@@ -8,6 +15,49 @@
         <el-button v-if="isAuth('publicmanagement:instrumentledger:list')" type="primary" @click="getDataList()">查询</el-button>
         <el-button v-if="isAuth('publicmanagement:instrumentledger:save')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
         <el-button v-if="isAuth('publicmanagement:instrumentledger:delete')"  type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
+         <!--Excel 参数导入 -->
+      <el-button
+        type="primary"
+        @click="showDialog = true"
+        v-if="isAuth('publicmanagement:instrumentledger:upload')"
+        ><i class="fa fa-download"></i>导入Excel文件
+      </el-button>
+      <el-dialog
+        title="导入Excel文件"
+        :visible.sync="showDialog"
+        width="30%"
+        :before-close="handleClose"
+        @close="resetFileInput"
+      >
+        <!-- <el-upload
+          class="upload-demo"
+          :on-success="handleUploadSuccess"
+          drag
+          action="#"
+          :before-upload="beforeUpload"
+          :show-file-list="false"
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击导入</em></div>
+        </el-upload> -->
+
+        <!-- el-upload 的方法暂时无法测试通过，使用input-file进行传到后端 -->
+        <i class="el-icon-upload"></i>
+        <input type="file" id="inputFile" ref="fileInput" @change="checkFile" />
+
+        <!-- 进度动画条 -->
+        <div v-if="progress > 0">
+          <el-progress
+            :percentage="progress"
+            color="rgb(19, 194, 194)"
+          ></el-progress>
+        </div>
+
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="showDialog = false">取 消</el-button>
+          <el-button type="primary" @click="fileSend()">确 定</el-button>
+        </span>
+      </el-dialog>
       </el-form-item>
     </el-form>
     <el-table
@@ -160,12 +210,19 @@
 
 <script>
   import AddOrUpdate from './instrumentledger-add-or-update'
+   import { listIndicatorSummary, uploadImports} from '@/api/indicator/indicator.js'
+  import { Notification } from 'element-ui'; // 引入 Notification 组件  
+  
+
   export default {
     data () {
       return {
+        alerts: [],
         dataForm: {
           key: ''
         },
+        showDialog: false,
+        progress: 0,
         dataList: [],
         pageIndex: 1,
         pageSize: 10,
@@ -175,13 +232,127 @@
         addOrUpdateVisible: false
       }
     },
+
     components: {
       AddOrUpdate
     },
     activated () {
       this.getDataList()
     },
+     mounted() {
+    this.getMessageList();
+  },
     methods: {
+// 在methods中添加以下方法
+acknowledgeAlert(alert) {
+  alert.state = 0;
+  this.updateMessageState(alert.numberId, 0);
+},
+
+updateMessageState(numberId, newState) {
+  this.$http({
+    url: this.$http.adornUrl(`/publicmanagement/instrumentledger/updatemessage`),
+    method: 'post',
+    data: this.$http.adornData({ numberId, state: newState }),
+  }).then(({ data }) => {
+    if (data && data.code === 0) {
+      // 更新成功，可以在这里做进一步处理
+    }
+  });
+},
+
+
+      //告警信息
+      
+getMessageList() {
+    
+
+  this.$http({
+    url: this.$http.adornUrl('/publicmanagement/instrumentledger/message'),
+    method: 'post',
+  }).then(({ data }) => {
+      if (data) {
+        data.forEach(item => {
+          if (item.state === 1) {
+            this.showAlert(item.numberId);
+          }
+        });
+      }
+    });
+  },
+
+  showAlert(numberId) {
+    // 在这里实现展示警告信息的功能
+    // 例如，使用Element UI的$message组件来显示警告信息
+    this.$message({
+      message: `警告：编号为 ${numberId} 的项需要关注！`,
+      type: 'warning',
+      duration: 3000
+    });
+  },
+
+
+       //导入excel，取消按钮绑定取消所选的xlsx
+    resetFileInput() {
+      this.$refs.fileInput.value = "";
+    },
+   checkFile() {
+      const file = this.$refs.fileInput.files[0];
+      const fileName = file.name;
+      const fileExt = fileName.split(".").pop(); // 获取文件的扩展名
+
+      if (fileExt !== "xlsx" && fileExt !== "xlsm") {
+        this.$message.error("只能上传 Excel 文件！");
+        this.$refs.fileInput.value = ""; // 清空文件选择框
+      }
+    },
+    // excel 导入
+  fileSend() {
+        const formData = new FormData();
+        const file = document.getElementById("inputFile").files[0]; // 获取文件对象
+        // console.log(file);
+        formData.append("file", file);
+//         // console.log("file====>",formData)
+        // axios({
+        //   method: "post",
+        //   // this $axios.post,
+        //   url: "http://localhost:8080/market/ledger/import",
+        //   // params:{
+        //   //   userName: this.$store.state.user.name,
+        //   // },
+        //   headers: {
+        //     "Content-Type": "multipart/form-data",
+        //   },
+        //   withCredentials: true,
+        //   data: formData,
+        //   onUploadProgress: (progressEvent) => {
+        //     this.progress = Math.round(
+        //       (progressEvent.loaded * 100) / progressEvent.total
+        //     );
+        //   },
+        // });
+console.log("file====>",formData)
+   const  url=this.$http.adornUrl(`/publicmanagement/instrumentledger/import`)
+        uploadImports(formData,url)
+console.log("file====>",formData)
+        .then(response => {
+          // 文件上传成功
+
+          setTimeout(() => {
+            this.showDialog = false; // 关闭上传面板
+            location.reload(); // 调用此方法刷新页面数据
+          }, 5000); // 2000毫秒后关闭
+          this.$message.success("上传成功");
+        })
+        .catch(error => {
+          // 处理错误
+          console.error('Error uploading file:', error);
+        });
+
+        
+
+    },
+
       // 获取数据列表
       getDataList () {
         this.dataListLoading = true
